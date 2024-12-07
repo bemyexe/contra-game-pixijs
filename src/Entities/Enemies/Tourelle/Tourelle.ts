05f945e09db9da127f89d88a53416c158bf21ddc
maxim <trifonov.max@mail.ru>
2024-12-07 12:13:19 +0300

    ref

--- src/Entities/Enemies/Tourelle/Tourelle.ts
+++ src/Entities/Enemies/Tourelle/Tourelle.ts
@@ -14,10 +14,19 @@ export default class Tourelle extends Entity {
 
     this.target = target;
     this.bulletFactory = bulletFactory;
+    this.isActive = false;
   }
 
   update() {
     if (this.target.isDead) return;
+
+    if (!this.isActive) {
+      if (this.x - this.target.x < 512 + this.collisionBox.width * 2) {
+        this.isActive = true;
+      }
+      return;
+    }
+
     let angle = Math.atan2(this.target.y - this.y, this.target.x - this.x);
     this._view.gunRotation = angle;
     this.fire(angle);
@@ -34,12 +43,12 @@ export default class Tourelle extends Entity {
   private fire(angle: number) {
     this.timeCounter++;
     if (this.timeCounter < 100) return;
-    const bulletContext: {[key: string]: number & string} = {
-      x: this.x,
-      y: this.y,
-      angle: (angle / Math.PI) * 180,
-      type: "enemyBullet",
-    };
+    const bulletContext: {[key: string]: any} = {};
+    bulletContext.x = this.x;
+    bulletContext.y = this.y;
+    bulletContext.angle = (angle / Math.PI) * 180;
+    bulletContext.type = "enemyBullet";
+
     this.bulletFactory.createBullet(bulletContext);
 
     this.timeCounter = 0;
--- src/Entities/Platforms/PlatformFactory.ts
+++ src/Entities/Platforms/PlatformFactory.ts
@@ -1,26 +1,104 @@
-import {Container} from "pixi.js";
+import {Graphics} from "pixi.js";
+import PlatformView from "./PlatformView";
 import Platform from "./Platform";
-import Box from "./Box";
+import BridgePlatform from "./BridgePlatform";
+import World from "../../World";
 
 export default class PlatformFactory {
+  private platformWidth = 128;
+  private platformHeight = 24;
   private worldContainer;
-
-  constructor(worldContainer: Container) {
+  constructor(worldContainer: World) {
     this.worldContainer = worldContainer;
   }
   createPlatform(x: number, y: number) {
-    const platform = new Platform();
+    const skin = new Graphics();
+    skin.stroke(0x004220);
+    skin.fill(0x00ff00);
+    skin.rect(0, 0, this.platformWidth, this.platformHeight);
+    skin.fill(0x694216);
+    skin.rect(
+      0,
+      this.platformHeight,
+      this.platformWidth,
+      this.platformHeight * 20
+    );
+    const view = new PlatformView(this.platformWidth, this.platformHeight);
+    view.addChild(skin);
+    const platform = new Platform(view);
     platform.x = x;
     platform.y = y;
-    this.worldContainer.addChild(platform);
+    this.worldContainer.background.addChild(view);
     return platform;
   }
-
   createBox(x: number, y: number) {
-    const box = new Box();
-    box.x = x;
-    box.y = y;
-    this.worldContainer.addChild(box);
+    const skin = new Graphics();
+    skin.stroke(0x004220);
+    skin.fill(0x00ff00);
+    skin.rect(0, 0, this.platformWidth, this.platformHeight);
+    skin.lineTo(this.platformWidth, this.platformHeight);
+    skin.fill(0x694216);
+    skin.rect(
+      0,
+      this.platformHeight,
+      this.platformWidth,
+      this.platformHeight * 20
+    );
+    const view = new PlatformView(this.platformWidth, this.platformHeight);
+    view.addChild(skin);
+    const platform = new Platform(view);
+    platform.x = x;
+    platform.y = y;
+    platform.type = "box";
+    this.worldContainer.background.addChild(view);
+    return platform;
+  }
+  createStepBox(x: number, y: number) {
+    const box = this.createBox(x, y);
+    box.isStep = true;
     return box;
+  }
+  createWater(x: number, y: number) {
+    const skin = new Graphics();
+    skin.stroke(0x0000ff);
+    skin.fill(0x0000ff);
+    skin.rect(0, -this.platformHeight, this.platformWidth, this.platformHeight);
+    skin.lineTo(this.platformWidth, this.platformHeight);
+    const view = new PlatformView(this.platformWidth, this.platformHeight);
+    view.addChild(skin);
+    const platform = new Platform(view);
+    platform.x = x;
+    platform.y = y;
+    platform.type = "box";
+    this.worldContainer.foreground.addChild(view);
+    return platform;
+  }
+  createBossWall(x: number, y: number) {
+    const skin = new Graphics();
+    skin.stroke(0x0000ff);
+    skin.fill(0x0b1e0f2);
+    skin.rect(0, 0, this.platformWidth * 3, 600);
+    skin.lineTo(this.platformWidth * 3, 600);
+    const view = new PlatformView(this.platformWidth * 3, 768);
+    view.addChild(skin);
+    const platform = new Platform(view);
+    platform.x = x;
+    platform.y = y;
+    platform.type = "box";
+    this.worldContainer.background.addChild(view);
+    return platform;
+  }
+  createBridge(x: number, y: number) {
+    const skin = new Graphics();
+    skin.stroke(0x111111);
+    skin.fill(0xffffff);
+    skin.rect(0, 0, this.platformWidth, this.platformHeight * 3);
+    const view = new PlatformView(this.platformWidth, this.platformHeight);
+    view.addChild(skin);
+    const platform = new BridgePlatform(view);
+    platform.x = x;
+    platform.y = y;
+    this.worldContainer.background.addChild(view);
+    return platform;
   }
 }
